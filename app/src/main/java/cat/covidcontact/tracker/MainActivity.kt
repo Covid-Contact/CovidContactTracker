7111ea5410b7f8c294201b3a13f765b404c465c1
Albert Pinto <apintogil@gmail.com>
2021-03-04 17:12:08 +0100

    Start branch

--- app/src/main/java/cat/covidcontact/tracker/MainActivity.kt
+++ app/src/main/java/cat/covidcontact/tracker/MainActivity.kt
@@ -1,11 +1,11 @@
 package cat.covidcontact.tracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) //
         setContentView(R.layout.activity_main)
     }
 }
