97db42eb277f35d1a2ec4937b352d98d9fc81cc2
Albert Pinto <apintogil@gmail.com>
2021-04-29 19:07:21 +0200

    Add search interface.

--- app/src/main/java/cat/covidcontact/tracker/feature/search/SearchFragment.kt
+++ app/src/main/java/cat/covidcontact/tracker/feature/search/SearchFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import cat.covidcontact.tracker.common.BaseFragment
+import cat.covidcontact.tracker.common.extensions.observeList
 import cat.covidcontact.tracker.common.handlers.ScreenStateHandler
 import cat.covidcontact.tracker.databinding.FragmentSearchBinding
+import cat.covidcontact.tracker.feature.contactnetworks.recyclerview.NetworkCardStateColor
+import cat.covidcontact.tracker.feature.contactnetworks.recyclerview.NetworkCardStateText
+import cat.covidcontact.tracker.feature.search.recycler.ContactNetworkSearchAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment() {
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapter: ContactNetworkSearchAdapter
+
     override val viewModel: SearchViewModel by viewModels()
     override val screenStateHandler = ScreenStateHandler<SearchState> { context, state ->
 
@@ -29,5 +36,20 @@ class SearchFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.bind()
+    }
+
+    private fun FragmentSearchBinding.bind() {
+        adapter = ContactNetworkSearchAdapter(
+            requireContext(),
+            NetworkCardStateColor(),
+            NetworkCardStateText()
+        ) { contactNetwork ->
+
+        }
+
+        contactNetworkSearchList.adapter = adapter
+        contactNetworkSearchList.layoutManager = LinearLayoutManager(requireContext())
+        contactNetworkSearchList.observeList(viewLifecycleOwner, viewModel.contactNetworks)
     }
 }
--- app/src/main/java/cat/covidcontact/tracker/feature/search/SearchViewModel.kt
+++ app/src/main/java/cat/covidcontact/tracker/feature/search/SearchViewModel.kt
@@ -1,5 +1,8 @@
 package cat.covidcontact.tracker.feature.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import cat.covidcontact.model.ContactNetwork
 import cat.covidcontact.tracker.common.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,5 +11,7 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
 
 ) : BaseViewModel() {
-
+    private val _contactNetworks = MutableLiveData<List<ContactNetwork>>(emptyList())
+    val contactNetworks: LiveData<List<ContactNetwork>>
+        get() = _contactNetworks
 }
