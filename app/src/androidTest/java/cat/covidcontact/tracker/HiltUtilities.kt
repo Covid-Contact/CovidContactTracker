3e8f9db66d2035bc7f6990dbbb7a4945826a68dd
Albert Pinto <apintogil@gmail.com>
2021-06-22 02:53:42 +0200

    Update version.

--- app/src/androidTest/java/cat/covidcontact/tracker/HiltUtilities.kt
+++ app/src/androidTest/java/cat/covidcontact/tracker/HiltUtilities.kt
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2021  Albert Pinto
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package cat.covidcontact.tracker
 
 import android.content.ComponentName
@@ -10,7 +27,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 
 const val THEME_EXTRAS_BUNDLE_KEY = "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity" +
-        ".THEME_EXTRAS_BUNDLE_KEY"
+    ".THEME_EXTRAS_BUNDLE_KEY"
 
 inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
--- app/src/main/java/cat/covidcontact/tracker/feature/userinfo/UserInfoFragment.kt
+++ app/src/main/java/cat/covidcontact/tracker/feature/userinfo/UserInfoFragment.kt
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2021  Albert Pinto
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package cat.covidcontact.tracker.feature.userinfo
 
 import android.os.Bundle
@@ -5,8 +22,6 @@ import android.view.View
 import androidx.fragment.app.activityViewModels
 import cat.covidcontact.model.user.User
 import cat.covidcontact.tracker.common.BaseFragment
-import cat.covidcontact.tracker.common.extensions.navigate
-import cat.covidcontact.tracker.common.extensions.navigateUp
 import cat.covidcontact.tracker.common.handlers.ScreenStateHandler
 import cat.covidcontact.tracker.feature.userinfo.fragments.CovidInformationFragmentDirections
 
